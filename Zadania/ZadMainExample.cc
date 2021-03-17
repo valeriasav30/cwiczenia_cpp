77a5c00af7aab06a5ce477be0d63a5f01b52aa9a
valeriasav30 <74907652+valeriasav30@users.noreply.github.com>
2021-03-17 15:40:55 +0100

    Update ZadMainExample.cc

--- Zadania/ZadMainExample.cc
+++ Zadania/ZadMainExample.cc
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+void One() {
+	cout << "Mam" << endl;
+}
+
+void Two() {
+	cout << "5" << endl;
+}
+
+void Three() {
+	cout << "funkcje!" << endl;
+}
+
+extern "C" void Four(){
+	printf("Print code");
+}
+
+extern "C" void Five();
+
+int main(int argc, char *argv[]) {
+	for (int i = 1; i<argc; i++) {
+		string c = argv[i];
+		if (c == "One") { One();}
+		else if (c == "Two"){Two();}
+		else if (c == "Three"){Three();}
+		else if (c == "Four"){Four();}
+		else if (c == "Five"){Five();}
+	}
+	return 0;
+}
