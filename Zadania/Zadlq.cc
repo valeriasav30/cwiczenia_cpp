d606291238c3207a9df014f64c15b17614a6786d
valeriasav30 <74907652+valeriasav30@users.noreply.github.com>
2021-04-05 15:49:55 +0200

    Create Zadlq.cc

---
+++ Zadania/Zadlq.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+  
+{
+  
+  int arr[] = {0, 1, 2, 3, 4};
+  
+  int i = 0;
+
+  for(i;i<5;i++)
+    
+  {
+    cout << i[arr] << endl;
+    
+  }
+
+  return 0;
+}
