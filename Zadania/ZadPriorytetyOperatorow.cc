4591cf96c28445a7cfec94775a09663a7f020ac9
valeriasav30 <74907652+valeriasav30@users.noreply.github.com>
2021-04-14 20:56:16 +0200

    Create ZadPriorytetyOperatorow.cc

---
+++ Zadania/ZadPriorytetyOperatorow.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void main() {
+	int x = 1;
+	int y = 2;
+	int z = 3;
+	int result[] {4,4,4,4};
+
+	result[0] *= -++x*x--+-y--%++z;
+	cout <<"0. "<< result[0] << endl;
+
+	result[1] *= -(++x)*(x--)+-(y--)%(++z);
+	cout <<"1. "<< result[1] << endl;
+
+	result[2] *= (-(++x))*(x--)+(-(y--))%(++z);
+	cout <<"2. "<< result[2] << endl;
+
+	result[3] *= ((-(++x))*(x--))+((-(y--))%(++z));
+	cout <<"3. "<< result[3] << endl;
+
+	return 0;
+}
